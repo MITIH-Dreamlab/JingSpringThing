fc95b3668ed736ca6fdc7d9b61db6ed3e4b2301e
John O'Hare <john@xrsystems.uk>
2024-07-16 11:55:16 +0100

    add a screenshot

--- webxr-graph/start_docker.sh
+++ webxr-graph/start_docker.sh
@@ -1,2 +1,8 @@
+export PORT_MAPPING=8443:8443
 docker build -t webxr-graph .
 docker run -d -p 8443:8443 -v $(pwd)/processed_files:/usr/src/app/data/processed_files webxr-graph
+export PORT_MAPPING=8443:8443
+
+docker run -d --name logseqXR:latest -p $PORT_MAPPING \
+  -v $(pwd)/processed_files:/usr/src/app/data/processed_files:rw \
+  webxr-graph
