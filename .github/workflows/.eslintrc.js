99ea083e0297661a4492bfd254f5f7ccb77b0664
flossverse <github@xrsystems.uk>
2024-08-23 11:06:48 +0100

    linter

---
+++ .github/workflows/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+    },
+    extends: 'eslint:recommended',
+    parserOptions: {
+      ecmaVersion: 12,
+      sourceType: 'module',
+    },
+    rules: {
+      // Add any specific rules here
+    },
+  };
+  
