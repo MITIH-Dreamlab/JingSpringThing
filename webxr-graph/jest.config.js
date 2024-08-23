6a80b58fe7789e472dbe8ca2d80ab394587a517f
flossverse <github@xrsystems.uk>
2024-08-23 11:44:48 +0100

    test update

--- webxr-graph/jest.config.js
+++ webxr-graph/jest.config.js
@@ -1,9 +1,14 @@
-module.exports = {
-    testEnvironment: 'node',
+// jest.config.js
+
+export default {
+    testEnvironment: 'jsdom',
     transform: {
-      '^.+\\.js$': 'babel-jest',
+      '^.+\\.js$': 'babel-jest'
     },
     moduleFileExtensions: ['js', 'json'],
     testPathIgnorePatterns: ['/node_modules/', '/public/'],
+    moduleNameMapper: {
+      '^three$': '<rootDir>/node_modules/three/build/three.module.js'
+    }
   };
   
