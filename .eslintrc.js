126a61ca2f16142d60aaf86898c29c850d4e7d2c
flossverse <github@xrsystems.uk>
2024-08-23 11:38:25 +0100

    test updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,16 @@
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
-      node: true,
-      jest: true
-    },
-    extends: 'eslint:recommended',
-    parserOptions: {
-      ecmaVersion: 12,
-      sourceType: 'module'
-    },
-    rules: {
-      // Add any specific rules here
-    }
-  };
-  
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module'
+  },
+  rules: {
+    // Add any specific rules here
+  }
+};
