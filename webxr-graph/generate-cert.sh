736659898c56215c6d520983899a9bd92a4beaa6
flossverse <github@xrsystems.uk>
2024-09-07 22:18:07 +0100

    reset project

--- webxr-graph/generate-cert.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -nodes -subj "/CN=localhost"
-
-# Set permissions for key.pem
-chmod 664 key.pem
-
-# Set permissions for cert.pem (optional, but recommended)
-chmod 644 cert.pem
